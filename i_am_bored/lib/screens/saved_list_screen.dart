36d2a255733a808b1470c93e59b83f37ba5d7894
prabhaskumra <prabhas1998@gmail.com>
2020-08-02 17:15:12 -0700

    Update saved_list_screen.dart
    Items can be added to the list

--- i_am_bored/lib/screens/saved_list_screen.dart
+++ i_am_bored/lib/screens/saved_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:i_am_bored/models/bored_data.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/saved_list.dart';
@@ -9,17 +10,63 @@ class SavedListScreen extends StatefulWidget {
 }
 
 class _SavedListScreenState extends State<SavedListScreen> {
+  Widget getTextWidgets(List<BoredData> savedListItmes, var mediaQuery) {
+    List<Widget> list = new List<Widget>();
+    for (var i = 0; i < savedListItmes.length; i++) {
+      // print("I");
+      // print(i);
+      list.add(
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          elevation: 10,
+          child: Container(
+            // width: 0,
+            padding: EdgeInsets.all(10),
+            child: InkWell(
+              onTap: () {},
+              child: SizedBox(
+                height: mediaQuery.height / 12,
+                child: Text(
+                  "${i + 1}. " + savedListItmes[i].activity,
+                  softWrap: true,
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                  // textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+          ),
+        ),
+        // Divider(),
+
+        // Divider(),
+      );
+    }
+    return new ListView(
+      padding: EdgeInsets.all(10),
+      children: list,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _mediaQuery = MediaQuery.of(context).size;
+
     final savedActivityList = Provider.of<SavedList>(context);
 
     return Container(
-      child: Center(
-        child: Text("No Saved Activities!"),
-        // child: ListView(
-        //   children: <Widget>[Text("${savedActivityList.savedItems[0].activity}")],
-        // ),
-      ),
+      child: getTextWidgets(savedActivityList.savedItems, _mediaQuery),
     );
   }
 }
+
+// class BuildSavedList extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return(
+
+//     );
+//   }
+// }
